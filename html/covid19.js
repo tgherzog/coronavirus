fbec2120dd3ab9423f3d9a5496d529c5398dce76
Tim Herzog <therzog1@worldbank.org>
2020-04-28 13:40:17 -0400

    better color palette

--- html/covid19.js
+++ html/covid19.js
@@ -11,19 +11,20 @@ chartColors = {
 
 // inspired by http://godsnotwheregodsnot.blogspot.com/2012/09/color-distribution-methodology.html
 moreChartColors = [
-"#000000", "#FFFF00", "#1CE6FF", "#FF34FF", "#FF4A46", "#008941", "#006FA6", "#A30059",
-"#FFDBE5", "#7A4900", "#0000A6", "#63FFAC", "#B79762", "#004D43", "#8FB0FF", "#997D87",
-"#5A0007", "#809693", "#FEFFE6", "#1B4400", "#4FC601", "#3B5DFF", "#4A3B53", "#FF2F80",
+"#1CE6FF", "#FF34FF", "#FF4A46", "#008941", "#006FA6", "#A30059",
+"#7A4900", "#0000A6", "#63FFAC", "#B79762", "#004D43", "#8FB0FF", "#997D87",
+"#5A0007", "#809693", "#1B4400", "#4FC601", "#3B5DFF", "#4A3B53", "#FF2F80",
 "#61615A", "#BA0900", "#6B7900", "#00C2A0", "#FFAA92", "#FF90C9", "#B903AA", "#D16100",
-"#DDEFFF", "#000035", "#7B4F4B", "#A1C299", "#300018", "#0AA6D8", "#013349", "#00846F",
-"#372101", "#FFB500", "#C2FFED", "#A079BF", "#CC0744", "#C0B9B2", "#C2FF99", "#001E09",
-"#00489C", "#6F0062", "#0CBD66", "#EEC3FF", "#456D75", "#B77B68", "#7A87A1", "#788D66",
-"#885578", "#FAD09F", "#FF8A9A", "#D157A0", "#BEC459", "#456648", "#0086ED", "#886F4C",
+"#000035", "#7B4F4B", "#A1C299", "#300018", "#0AA6D8", "#013349", "#00846F",
+"#372101", "#FFB500", "#A079BF", "#CC0744", "#C0B9B2", "#001E09",
+"#00489C", "#6F0062", "#0CBD66", "#456D75", "#B77B68", "#7A87A1", "#788D66",
+"#885578", "#FF8A9A", "#D157A0", "#BEC459", "#456648", "#0086ED", "#886F4C",
 "#34362D", "#B4A8BD", "#00A6AA", "#452C2C", "#636375", "#A3C8C9", "#FF913F", "#938A81",
-"#575329", "#00FECF", "#B05B6F", "#8CD0FF", "#3B9700", "#04F757", "#C8A1A1", "#1E6E00",
+"#575329", "#B05B6F", "#8CD0FF", "#3B9700", "#C8A1A1", "#1E6E00",
 "#7900D7", "#A77500", "#6367A9", "#A05837", "#6B002C", "#772600", "#D790FF", "#9B9700",
-"#549E79", "#FFF69F", "#201625", "#72418F", "#BC23FF", "#99ADC0", "#3A2465", "#922329",
-"#5B4534", "#FDE8DC", "#404E55", "#0089A3", "#CB7E98", "#A4E804", "#324E72", "#6A3A4C",
+"#549E79", "#201625", "#72418F", "#BC23FF", "#99ADC0", "#3A2465", "#922329",
+"#5B4534", "#404E55", "#0089A3", "#CB7E98", "#A4E804", "#324E72", "#6A3A4C",
+"#000000",
 ];
 
 trendLen = 7; // also some static labels in the HTML that need to match any changes here
@@ -676,7 +677,7 @@ function initialize() {
         while( data.dayOffset < data['states']['USA']['cases'].length && data['states']['USA']['cases'][data.dayOffset] < 25 )
             data.dayOffset++;
 
-        var i = 2, stats = {}, case_list = [];
+        var i = 0, stats = {}, case_list = [];
         var default_state = 'Virginia';
         $table = $('#state-table').DataTable();
         for(var key in data['states']) {
@@ -753,11 +754,11 @@ function initialize() {
 
 
         $table = $('#topcounty-table').DataTable();
-        i = 2;
+        i = 0;
         case_list = [];
         for(var key in data['counties']) {
             // assign constant chart colors for states
-            if( i == moreChartColors.length ) i = 2;
+            if( i == moreChartColors.length ) i = 0;
 
             data['counties'][key]['lineColor'] = moreChartColors[i++];
             
